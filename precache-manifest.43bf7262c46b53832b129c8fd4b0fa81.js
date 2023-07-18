fc87ae2997e19a5538cb48d56a83dd955258d0c5
dan <dan@pop-os.localdomain>
2023-07-18 18:27:27 +0300

    allo

---
+++ precache-manifest.43bf7262c46b53832b129c8fd4b0fa81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af77b57c8f38a6a898ad",
+    "url": "/ppd/static/css/main.28884e61.chunk.css"
+  },
+  {
+    "revision": "af77b57c8f38a6a898ad",
+    "url": "/ppd/static/js/main.af77b57c.chunk.js"
+  },
+  {
+    "revision": "09c987824597cc7742e9",
+    "url": "/ppd/static/js/1.09c98782.chunk.js"
+  },
+  {
+    "revision": "8f8c423d308ffe55cf37",
+    "url": "/ppd/static/js/runtime~main.8f8c423d.js"
+  },
+  {
+    "revision": "3c372a74a65df0c915deb08d9fbdac3f",
+    "url": "/ppd/index.html"
+  }
+];
